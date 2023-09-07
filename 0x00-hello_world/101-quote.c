a74ed70ac7b6811b0e6dd66e0d5ac7055707f434
yahia912 <yahiaboss2003@gmail.com>
2023-09-08 00:45:15 +0300

    New file

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,7 @@
 int main(void)
 {
 		write(1, "and that piece of art is useful\" - Dora Korpar, ", 48);
-		write(1, "2015-10-19\n", 12);
+		write(1, "2015-10-19", 10);
+		write(1, "\n", 1);
 		return (1);
 }
