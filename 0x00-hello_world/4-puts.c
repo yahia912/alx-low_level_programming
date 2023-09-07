193c99d25a859499b8d553b335025f1a356cbc72
yahia912 <yahiaboss2003@gmail.com>
2023-09-07 21:54:07 +0300

    New file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
+/**
+ * main - outputs a string using puts
+ * Return: A string
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 
 }
 
