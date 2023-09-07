fd520a76bc5537a260f56eaed1f98e4b993d9e31
yahia912 <yahiaboss2003@gmail.com>
2023-09-07 22:06:47 +0300

    New file

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-/*
+/**
  * main - prints a string using printf
  * Return: A string
  */
@@ -7,5 +7,4 @@ int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-
 }	
