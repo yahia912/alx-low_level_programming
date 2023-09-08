0cc1258779cd7d4de6f0c10fff1f802e954f23df
yahia912 <yahiaboss2003@gmail.com>
2023-09-09 00:50:05 +0300

    New file

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Print the alphabets in lowercase
+ * Return: 0(success)
+ */
+int main(void)
+{
+	char ch = 'a';
+	char chs = 'A';
+
+	for (; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	
+	for (; chs <= 'Z'; chs++)
+	{
+		putchar(chs);
+	}
+	putchar('\n');
+	return (0);
+}
