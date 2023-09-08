a98ddde9da51c7a759b633c1867f2f8ff356829a
yahia912 <yahiaboss2003@gmail.com>
2023-09-09 01:19:47 +0300

    New file

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints integres from 0 to 9
+ * Return: 0(success)
+ */
+int main(void)
+{
+	int i = 0;
+
+	for (; i <= 9; i++)
+	{
+		putchar(i + '0');
+	}
+	putchar('\n');
+	return (0);
+}
