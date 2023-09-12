ab166bd935a385dfc485878bdf99b1698993d2b1
yahia912 <yahiaboss2003@gmail.com>
2023-09-12 23:42:55 +0300

    New file

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * main - Function that prints the alphabet in lowercase
+ * print_alphabet - Function that prints the alphabet in lowercase
  * Return: void
  */
 void print_alphabet(void)
