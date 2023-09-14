fb8009719857858578ac67f5f883062ac9083a61
yahia912 <yahiaboss2003@gmail.com>
2023-09-15 02:54:13 +0300

    New file

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * more_numbers - nmbr fct
+ * Return: no return
+ */
+
+void more_numbers(void)
+{
+	int c;
+	int i;
+
+	for (c = 0; c <= 10; c++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i >= 10)
+			{
+				_putchar('1');
+				_putchar(i % 10 + '0');
+			}
+		}
+	}
+	_putchar('\n');
+}
