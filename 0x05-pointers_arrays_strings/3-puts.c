aea70b3fad773351e1c68af59e08a3ccae0375b0
yahia912 <yahiaboss2003@gmail.com>
2023-09-18 21:37:34 +0300

    New file

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * _puts - Prints string
+ * @str: String to be printed
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
