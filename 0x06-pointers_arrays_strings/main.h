37cdd8e258916c88bfe86e92259148e3398de41c
yahia912 <yahiaboss2003@gmail.com>
2023-09-22 02:04:21 +0300

    New file

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -8,4 +8,5 @@ void reset_to_98(int *n);
 int _strlen(char *s);
 char *_strcat(char *dest, char *src);
 void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 #endif
