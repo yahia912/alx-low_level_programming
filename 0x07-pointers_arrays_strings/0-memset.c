cb472169aaa97f221177443611a692b5ad9330e2
yahia912 <yahiaboss2003@gmail.com>
2023-09-26 00:39:20 +0300

    New file

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,6 +4,7 @@
  * @s: Points to a memory area to be filled
  * @b: Character to fill memory are with
  * @n: Fill how many bytes of memory area
+ * Return : char
  */
 char *_memset(char *s, char b, unsigned int n)
 {
@@ -13,5 +14,5 @@ char *_memset(char *s, char b, unsigned int n)
 	{
 		s[i] = b;
 	}
-	return(s);
+	return (s);
 }
