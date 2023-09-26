d43bb9978a0f153195ae13ccdb74afbdae7db396
yahia912 <yahiaboss2003@gmail.com>
2023-09-26 03:30:52 +0300

    New file

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_chessboard - Function that prints chessboard
+ * @a: array
+ * Return: void
+ */
+void print_chessboard(char (*a)[8])
+{
+	int x, y;
+
+	for (x = 0; x < 8; x++)
+	{
+		for (y = 0; y < 8; y++)
+		{
+			_putchar(a[x][y])
+		}
+	}
+}
