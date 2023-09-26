4dda95b32a52a54f1f78f6d598a10b76c3ec7b05
yahia912 <yahiaboss2003@gmail.com>
2023-09-26 03:19:26 +0300

    New file

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -10,9 +10,9 @@ char *_strpbrk(char *s, char *accept)
 	int i, j;
 	char *p;
 
-	for (i = 0; s[i] >= '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; accept[j]; j++)
+		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
 			{
