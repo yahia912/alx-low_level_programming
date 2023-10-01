a567b4fede797380b6b1705690cd214f6da4ec33
yahia912 <yahiaboss2003@gmail.com>
2023-10-02 02:02:38 +0300

    New file

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,13 +7,13 @@
 
 void _puts_recursion(char *s)
 {
-    if (*s == '\0')
-    {
-        _putchar('\n');
-    }
-    else
-    {
-        putchar(*s);
-        _puts_recursion(s + 1);
-    }
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		putchar(*s);
+		_puts_recursion(s + 1);
+	}
 }
