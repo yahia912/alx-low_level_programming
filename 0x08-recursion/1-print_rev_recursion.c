72399c8304bd013d676dbb7cd9124ef5a3a82bb0
yahia912 <yahiaboss2003@gmail.com>
2023-10-02 02:20:43 +0300

    New file

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+/**
+ * _print_rev_recursion - Revereses a string
+ * @s: string
+ * Return: void
+ */
+#include "main.h"
+
+void _print_rev_recursion(char *s)
+{
+	if (*s > '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
