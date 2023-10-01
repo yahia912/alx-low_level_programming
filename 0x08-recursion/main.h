aba10fabf68572bdb1aff4c241ff41b8422ff951
yahia912 <yahiaboss2003@gmail.com>
2023-10-02 01:51:04 +0300

    New file

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+int _putchar(char c);
+void reset_to_98(int *n);
+int _strlen(char *s);
+char *_strcat(char *dest, char *src);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
+void set_string(char **s, char *to);
+#endif
