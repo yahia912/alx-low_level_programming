29e14e82277645271e0dcf6c3e09339cfd7d44aa
yahia912 <yahiaboss2003@gmail.com>
2023-10-02 23:16:10 +0300

    New file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - length of string
+ * @argc:size of argv
+ * @argv: array of command line arguements
+ * Return:0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc - 1]);
+	return (0);
+}
