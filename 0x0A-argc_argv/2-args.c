915729b4f5e438d9564966d9d3c73661466dd598
yahia912 <yahiaboss2003@gmail.com>
2023-10-02 23:35:49 +0300

    New file

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints all arguements
+ * @argc:size of argv
+ * @argv: array of command line arguements
+ * Return:0
+ */
+int main(int argc, char *argv[])
+{
+	int i = 1;
+
+	for (; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
